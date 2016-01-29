969d4f30ddad561fc3d2e7bd2c39d3cb41e38258
satanson8517 <michal.nedbalek@gmail.com>
2016-01-29 14:10:59 +0100

    ADD, CHANGE fixed errors, classes copied from the original project

--- src/main/java/com/avg/mne/annotation_processing/example/FactoryAnnotatedClass.java
+++ src/main/java/com/avg/mne/annotation_processing/example/FactoryAnnotatedClass.java
@@ -1,5 +1,6 @@
 package com.avg.mne.annotation_processing.example;
 
+import java.util.Optional;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.MirroredTypeException;
@@ -10,66 +11,71 @@ import javax.lang.model.type.MirroredTypeException;
  */
 public class FactoryAnnotatedClass {
 
-  private TypeElement annotatedClassElement;
-  private String qualifiedSuperClassName;
-  private String simpleTypeName;
-  private String id;
+	private TypeElement annotatedClassElement;
+	private String qualifiedSuperClassName;
+	private String simpleTypeName;
+	private String id;
 
-  public FactoryAnnotatedClass(TypeElement classElement) throws IllegalArgumentException {
-    this.annotatedClassElement = classElement;
-    Factory annotation = classElement.getAnnotation(Factory.class);
-    id = annotation.id();
+	public FactoryAnnotatedClass(TypeElement classElement) throws IllegalArgumentException {
+		this.annotatedClassElement = classElement;
+		Factory annotation = classElement.getAnnotation(Factory.class);
+		id = annotation.id();
 
-    if (StringUtils.isEmpty(id)) {
-      throw new IllegalArgumentException(
-          String.format("id() in @%s for class %s is null or empty! that's not allowed",
-              Factory.class.getSimpleName(), classElement.getQualifiedName().toString()));
-    }
+		final Optional<String> o_id = Optional.ofNullable(id);
+		if (!o_id.isPresent()) {
+//		if (StringUtils.isEmpty(id)) {
+			throw new IllegalArgumentException(
+					String.format("id() in @%s for class %s is null or empty! that's not allowed",
+							Factory.class.getSimpleName(), classElement.getQualifiedName().toString()));
+		}
 
-    // Get the full QualifiedTypeName
-    try {
-      Class<?> clazz = annotation.type();
-      qualifiedSuperClassName = clazz.getCanonicalName();
-      simpleTypeName = clazz.getSimpleName();
-    } catch (MirroredTypeException mte) {
-      DeclaredType classTypeMirror = (DeclaredType) mte.getTypeMirror();
-      TypeElement classTypeElement = (TypeElement) classTypeMirror.asElement();
-      qualifiedSuperClassName = classTypeElement.getQualifiedName().toString();
-      simpleTypeName = classTypeElement.getSimpleName().toString();
-    }
-  }
+		// Get the full QualifiedTypeName
+		try {
+			Class<?> clazz = annotation.type();
+			qualifiedSuperClassName = clazz.getCanonicalName();
+			simpleTypeName = clazz.getSimpleName();
+		} catch (MirroredTypeException mte) {
+			DeclaredType classTypeMirror = (DeclaredType) mte.getTypeMirror();
+			TypeElement classTypeElement = (TypeElement) classTypeMirror.asElement();
+			qualifiedSuperClassName = classTypeElement.getQualifiedName().toString();
+			simpleTypeName = classTypeElement.getSimpleName().toString();
+		}
+	}
 
-  /**
-   * Get the id as specified in {@link Factory#id()}.
-   * return the id
-   */
-  public String getId() {
-    return id;
-  }
+	/**
+	 * Get the id as specified in {@link Factory#id()}. return the id
+	 *
+	 * @return
+	 */
+	public String getId() {
+		return id;
+	}
 
-  /**
-   * Get the full qualified name of the type specified in  {@link Factory#type()}.
-   *
-   * @return qualified name
-   */
-  public String getQualifiedFactoryGroupName() {
-    return qualifiedSuperClassName;
-  }
+	/**
+	 * Get the full qualified name of the type specified in
+	 * {@link Factory#type()}.
+	 *
+	 * @return qualified name
+	 */
+	public String getQualifiedFactoryGroupName() {
+		return qualifiedSuperClassName;
+	}
 
+	/**
+	 * Get the simple name of the type specified in {@link Factory#type()}.
+	 *
+	 * @return qualified name
+	 */
+	public String getSimpleFactoryGroupName() {
+		return simpleTypeName;
+	}
 
-  /**
-   * Get the simple name of the type specified in  {@link Factory#type()}.
-   *
-   * @return qualified name
-   */
-  public String getSimpleFactoryGroupName() {
-    return simpleTypeName;
-  }
-
-  /**
-   * The original element that was annotated with @Factory
-   */
-  public TypeElement getTypeElement() {
-    return annotatedClassElement;
-  }
+	/**
+	 * The original element that was annotated with @Factory
+	 *
+	 * @return
+	 */
+	public TypeElement getTypeElement() {
+		return annotatedClassElement;
+	}
 }
